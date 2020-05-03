e8fa78cc9730db13125e1d0ae7c20f1be9f58027
Srba Miljkovic <srba.miljkovic@gmail.com>
2020-05-03 11:56:47 +0200

    ne znam, brdo promena sam napravio, sada radi, ali ponekad nije u sekundu precizan jbg :)

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 function dodajPredmet(){
 
     var brojPredmeta = document.getElementsByClassName("predmet").length + 1;
-
-    console.log("ovo je broj predmeta: "+brojPredmeta);
-
-
-
+    
     var predmetHtml = '<div class="predmet"><button class="dugme ukloni" id="ukloni" href="#" onclick="ukloni(this)">X</button><div><input class="period" id="period1" type="time"><input class="subject" id="subject" type="text" placeholder="Naziv predmeta"></input></div></div>';
 
     $("#predmeti").append(predmetHtml);
@@ -17,66 +13,90 @@ function ukloni(e){
 
 
 
-function finish(){
-    
-    var predmeti = document.getElementsByClassName("predmeti");
+function start(){
 
-    var brojPredmeta = predmeti.length;
 
-    for(i=0; i<=brojPredmeta; i++){
 
-        var period1 = predmeti[i].getElementById("period1");
+    var razlikaLista = [];
+    var subjectLista = [];
 
-        var subject = predmeti[i].getElementById("subject").value;
+    var brojPredmeta = $(".predmet").length;
 
-        // var period2 = brojPredmeta[i].getElementById("period2");
+    var razlika = 0;
+    var prethodnaRazlika = 0;
 
+    for(i=0; i<brojPredmeta; i++){
+
+        console.log("usao sam u petlju "+i);
         
-        
-
         var period = new Date();
         var trenutno = new Date();
-        var minusSekunde = Date.now()%100000;
+        var minusSekunde = trenutno % 100000;
+        
 
-        period.setHours(document.getElementById("period1").value.split(":")[0]);
-        period.setMinutes(document.getElementById("period1").value.split(":")[1]);
+        razlika = 0;
 
-        var razlika = period - trenutno - minusSekunde;
+        period.setHours(document.getElementsByClassName("period")[i].value.split(":")[0]);
+        period.setMinutes(document.getElementsByClassName("period")[i].value.split(":")[1]);
 
-        if(razlika>=0){
-            setTimeout(nekaFunkcija(subject, period), razlika);
+        console.log("period: "+period);
+        console.log("trenutno: "+trenutno);
+        console.log("minusSekunde: "+minusSekunde);
+        console.log("prethodnaRazlika: "+prethodnaRazlika);
+
+
+        razlika = period - trenutno
+        razlika = razlika - minusSekunde
+        razlika = razlika - prethodnaRazlika;
+
+        var subject = $(".subject:eq("+i+")").val();
+
+        razlikaLista.push(razlika);
+
+        subjectLista.push(subject);
+
+        console.log("razlika: "+razlika);
+
+        prethodnaRazlika += razlika;
+
+       
+
+    }
+
+
+    var brojac1 = 0;
+    var limiter1 = razlikaLista.length;
+
+    setTimeout(function(){manuelniLoop(brojac1, limiter1)}, razlikaLista[0]);
+
+    function manuelniLoop(brojac2, limiter2){
+        // console.log("Pre if-a Brojac2: "+brojac2);
+        // console.log("Pre if-a limiter2: "+limiter2);
+        if(brojac2 < limiter2){
+            console.log("razlika1: "+razlikaLista[brojac2]);
+            console.log("subject1: "+subjectLista[brojac2]);
+
+            overlayON(razlikaLista[brojac2], subjectLista[brojac2]);
+
+            brojac2++;
+
+            setTimeout(function(){manuelniLoop(brojac2, limiter2)}, razlikaLista[brojac2]);
+
         }
 
     }
 
-    
 }
 
+function overlayON(razlika, subject){
 
-
-function nekaFunkcija(subject, period){
-
-    console.log("ovo je subject koji je prosledjen nekoj funkciji: "+subject);
-    console.log("ovo je period koji je prosledjen nekoj funkciji: "+period);
-
-    $("#overlay").fadeOut("slow");
+    if(razlika>=0){
+        $("#overlay").css("display", "none");
     
-    
-    
-    new Audio("school-bell-sound.mp3").play();
-    
-    $("#subject").html
-    $("#overlay").fadeIn("slow");
+        new Audio("school-bell-sound.mp3").play();
+        $("#popup").html("<h2>"+subject+"</h2>");
+        $("#overlay").css("display", "block");
 
-}
-
-
-
-
-function test(){
-    
-    var childcount = document.getElementById("predmeti").getElementsByClassName("predmet").length;
-
-    console.log(childcount);
+    }
     
 }
